f774c642b9901890b75b06a50ce6270ca9790a3e
Mia <miabernhardt75@gmail.com>
2023-09-09 21:16:17 +0200

    fixed wrong comments

--- src/main.c
+++ src/main.c
@@ -51,6 +51,17 @@ int main(int argc, char **argv) {
   // Vertex Array Object
   //===
   unsigned int VAO;
+
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  //===
+  // Vertex Buffer Object
+  //===
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
   // [x, y, z] (position of the points of the triangle)
   float vertices[] = {
       -0.5f, -0.5f, 0.0f, // 1
@@ -58,20 +69,10 @@ int main(int argc, char **argv) {
       0.0f,  0.5f,  0.0f, // 3
   };
 
-  glGenVertexArrays(1, &VAO);
-  glBindVertexArray(VAO);
-
-  //===
-  // Vertex Array Buffer
-  //===
-  unsigned int VBO;
-  glGenBuffers(1, &VBO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-  // Load the vertices into the vertex array buffer
+  // Copy the date into the underlying array buffer (VAO)
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  // [y, y, z]
+  // [x, y, z] (tell openGL how to interpret the data in the array)
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
