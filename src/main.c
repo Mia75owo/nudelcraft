d0a5591468aa6172f9f6b590cd56dfdb323dca72
Mia <miabernhardt75@gmail.com>
2023-09-09 20:57:33 +0200

    normal triangle

--- src/main.c
+++ src/main.c
@@ -51,11 +51,11 @@ int main(int argc, char **argv) {
   // Vertex Array Object
   //===
   unsigned int VAO;
-  // [x, y, z]+[r, g, b]
+  // [x, y, z] (position of the points of the triangle)
   float vertices[] = {
-      -0.5f, -0.5f, 0.0f, 1.0, 0.0, 0.0, // 1
-      0.5f,  -0.5f, 0.0f, 0.0, 1.0, 0.0, // 2
-      0.0f,  0.5f,  0.0f, 0.0, 0.0, 1.0, // 3
+      -0.5f, -0.5f, 0.0f, // 1
+      0.5f,  -0.5f, 0.0f, // 2
+      0.0f,  0.5f,  0.0f, // 3
   };
 
   glGenVertexArrays(1, &VAO);
@@ -72,24 +72,8 @@ int main(int argc, char **argv) {
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
   // [y, y, z]
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
-  // [r, g, b] (with 3 floats of offset)
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
-                        (void *)(3 * sizeof(float)));
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
-  glEnableVertexAttribArray(1);
-
-  //===
-  // Element Buffer Object
-  //===
-  unsigned int EBO;
-  unsigned int indicies[] = {0, 1, 2};
-
-  glGenBuffers(1, &EBO);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies,
-               GL_STATIC_DRAW);
 
   //=============
   // main loop
@@ -105,7 +89,7 @@ int main(int argc, char **argv) {
 
     // Render the buffer
     glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -114,7 +98,6 @@ int main(int argc, char **argv) {
   // Cleanup
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VAO);
-  glDeleteBuffers(1, &EBO);
 
   shader_destroy(&shader);
 
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,9 +1,8 @@
 #version 330 core
 
 layout(location = 0) out vec4 diffuseColor;
-in vec3 vertexColor;
 
 void main() {
-  // Pass the color without modifying it
-  diffuseColor = vec4(vertexColor.xyz, 1.0);
+  // Set the color to red
+  diffuseColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
